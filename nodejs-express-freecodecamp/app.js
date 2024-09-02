69ac6f19a5bdf4ba53102482cb005bf6257bd033
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-02 16:16:23 +0200

    update app.js

--- nodejs-express-freecodecamp/app.js
+++ nodejs-express-freecodecamp/app.js
@@ -1,9 +1,12 @@
-const amount = 12
+// GLOBALS - NO WINDOW
 
-if(amount < 10) {
-    console.log("small number");
-} else {
-    console.log('Large number');
-}
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (commonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
 
-console.log(`Hey it's my first node app!!`);
+console.log(__dirname);
+setInterval(() => {
+    console.log("Hello world!");
+}, 1000);
