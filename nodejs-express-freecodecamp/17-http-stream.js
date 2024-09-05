001a768c4dee95dc9401bd77beb475cceee2a170
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-05 08:54:52 +0200

    refactor

--- nodejs-express-freecodecamp/17-http-stream.js
+++ nodejs-express-freecodecamp/17-http-stream.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var fs = require('fs')
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
     const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
