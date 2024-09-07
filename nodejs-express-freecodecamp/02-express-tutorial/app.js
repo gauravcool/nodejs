8f91143f105fe23aa89eae5b60746c27005b0962
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-07 13:35:24 +0200

    express basics

--- nodejs-express-freecodecamp/02-express-tutorial/app.js
+++ nodejs-express-freecodecamp/02-express-tutorial/app.js
@@ -1,48 +1,27 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express')
+const app = express()
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeLogo = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-
-const server = http.createServer((req, res) => {
-    console.log(req.method);
-    const url = req.url;
-    console.log(url);
-    
-    //home page
-    if(url==='/') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-    // styles
-    else if(url === '/styles.css') {
-        res.writeHead(200, {'content-type': 'text/css'})
-        res.write(homeStyles)
-        res.end()
-    }
-    // logo
-    else if(url === '/logo.svg') {
-        res.writeHead(200, {'content-type': 'image/svg+xml'})
-        res.write(homeLogo)
-        res.end()
-    }
-    // logo
-    else if(url === '/browser-app.js') {
-        res.writeHead(200, {'content-type': 'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-    }
-    // logic
-    else {
-        res.writeHead(404, {'content-type': 'text/html'})
-        res.write(homeLogic)
-        res.end()
-    }
-    
+app.get('/', (req, res) => {
+    console.log('User hit the resource');
+    res.status(200).send('Home Page')
 })
 
-server.listen(3000)
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found</h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000...');
+})
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
