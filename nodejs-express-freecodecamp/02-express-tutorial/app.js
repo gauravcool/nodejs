70e864bafa362c2996aaedd87d850ba1d9798ffd
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-08 04:49:29 +0200

    basic json

--- nodejs-express-freecodecamp/02-express-tutorial/app.js
+++ nodejs-express-freecodecamp/02-express-tutorial/app.js
@@ -1,24 +1,13 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
 
-// setup static and middleware
-app.use(express.static('./public'))
+const {products} = require('./data')
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-
-
-app.all('*', (req, res) => {
-    res.status(404).send('Resource not found')
+app.get('/', (req, res) => {
+    res.json(products)
 })
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000...');
+    console.log('Server is listening on port 3000');
 })
-
-//app.get
-
