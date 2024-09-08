c1ddafee0f08b095cd2cdc4187d31fd86f6fe56a
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-08 04:38:50 +0200

    express app

--- nodejs-express-freecodecamp/02-express-tutorial/app.js
+++ nodejs-express-freecodecamp/02-express-tutorial/app.js
@@ -1,27 +1,24 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('User hit the resource');
-    res.status(200).send('Home Page')
-})
+// setup static and middleware
+app.use(express.static('./public'))
 
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1>Resource not found</h1>')
+    res.status(404).send('Resource not found')
 })
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000...');
+    console.log('Server is running on port 3000...');
 })
 
 //app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
+
