d707531944d94a0d7507ff5c5969ada1a2e97382
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-09 17:26:14 +0200

    express middleware basics

---
+++ nodejs-express-freecodecamp/02-express-tutorial/README.md
@@ -0,0 +1,5 @@
+1. We use middleware to add some functionalities. 
+
+2. We can inclde it as 2nd argument in the get call.
+
+3. No need to explicitly pass the parameters (request, response and next)
--- nodejs-express-freecodecamp/02-express-tutorial/app.js
+++ nodejs-express-freecodecamp/02-express-tutorial/app.js
@@ -2,55 +2,27 @@ const express = require('express')
 
 const app = express()
 
-const {products} = require('./data')
+//req -> middleware -> res
+
+
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products/2">products</a>')
+    res.send('Home')
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image}
-    });
-    res.json(newProducts);
-})
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time);
+    next()
+}
 
-app.get('/api/products/:productId', (req, res) => {
-    console.log('jkk',req.params);
+app.get('/about', logger, (req, res) => {
     
-    const singleProduct = products.find((product)=> product.id === Number(req.params.productId));
-    if(!singleProduct) {
-        res.status(404).send('Product does not exist');
-    }
-    res.json(singleProduct);
-})
-
-app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
-    console.log(req.params);
-    res.send('Hello World')
-})
-
-app.get('/api/v1/query', (req, res) => {
-    // console.log(req.query);
-    const {search, limit} = req.query;
-    let sortedProducts = [...products];
-    console.log(sortedProducts);
-    
-    if(search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search);
-        })
-    }
-    if(limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if(sortedProducts.length < 1) {
-        return res.status(404).send({success:true, data: []});
-    }
-    return res.status(200).json(sortedProducts);
+    res.send(`About`);
 })
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000...');
 })
