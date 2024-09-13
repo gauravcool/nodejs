6fb5d6fa4e51dda3a94ee4d0aaef3fc01953143e
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-09-13 14:48:09 +0200

    express middleware use method

--- nodejs-express-freecodecamp/02-express-tutorial/app.js
+++ nodejs-express-freecodecamp/02-express-tutorial/app.js
@@ -1,26 +1,33 @@
 const express = require('express')
-
+const logger = require('./logger')
 const app = express()
 
 //req -> middleware -> res
-
+app.use('/api', logger);
 
 
 app.get('/', (req, res) => {
     res.send('Home')
 })
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next()
-}
+// const logger = (req, res, next) => {
+//     const method = req.method;
+//     const url = req.url;
+//     const time = new Date().getFullYear();
+//     console.log(method, url, time);
+//     next()
+// }
 
-app.get('/about', logger, (req, res) => {
-    
+app.get('/about', (req, res) => {
     res.send(`About`);
+})
+
+app.get('/api/products', (req, res) => {
+    res.send(`Products`);
+})
+
+app.get('/api/items', (req, res) => {
+    res.send(`Items`);
 })
 
 app.listen(3000, () => {
