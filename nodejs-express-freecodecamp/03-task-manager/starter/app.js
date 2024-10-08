c2de36618a443b04d1598fcf80df743f96f95e1f
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-10-08 11:49:32 +0200

    set up app js with server

--- nodejs-express-freecodecamp/03-task-manager/starter/app.js
+++ nodejs-express-freecodecamp/03-task-manager/starter/app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+const tasks = require('./routes/tasks')
+
+//middleware
+app.use(express.json())
+
+app.use('/api/v1/tasks', tasks)
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Task Manager App')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
